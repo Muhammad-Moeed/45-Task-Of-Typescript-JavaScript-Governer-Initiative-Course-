c629254f33f8d20ecd4a602c391f6c8bb76b6fd0
Muhammad Moeed <134531518+Muhammad-Moeed@users.noreply.github.com>
2024-02-25 16:34:53 +0500

    ass2.ts

--- ass2.ts
+++ ass2.ts
@@ -1,18 +1,21 @@
-// var Name = "Moeed";
-// var uppercase = Name.toUpperCase();
-// console.log(uppercase);
+//Upper Case 
 
-// var Name = "Moeed";
-// var lowercase = Name.toLowerCase();
-// console.log(lowercase);
+var Name = "Moeed Qadri";
+var uppercase = Name.toUpperCase();
+console.log(uppercase);
 
+//Lower Case 
 
-// function toTitleCase(str) {
-//     return str.replace(/\w\S*/g, function(txt) {
-//         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-//     });
-// }
+var Name = "Moeed Qadri";
+var lowercase = Name.toLowerCase();
+console.log(lowercase);
 
-// var point = "the world is circle";
-// var titlecase = toTitleCase(point);
-// console.log(titlecase);
+//Title Case 
+
+var Name:string = "Moeed Qadri";
+var words:string[] = Name.split(" ");
+var Titlecase:string="";
+for(var i = 0;i<words.length;i++){
+    Titlecase += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
+}
+console.log(Titlecase.trim());
