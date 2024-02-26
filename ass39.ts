fc4edbd4040ef418f977c4a56d71a6a67a142cf9
Muhammad Moeed <134531518+Muhammad-Moeed@users.noreply.github.com>
2024-02-26 16:27:07 +0500

    ass39.ts

--- ass39.ts
+++ ass39.ts
@@ -1,3 +1,5 @@
+//Album:
+
 function make_album(artist_name: string, title: string, track: number | null = null): { artist: string, title: string, track: number | null } {
     var album = { artist: artist_name, title: title, track: track };
     return album;
